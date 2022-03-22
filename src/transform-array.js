dad9a59dc29e8f01a402eb662dc67939fc784508
Igor M <terra.kot.ig@gmail.com>
2022-03-22 12:52:11 +0300

    feat: transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,54 @@
-const { NotImplementedError } = require('../extensions/index.js');
+/**
+ * @typedef  {'--discard-next'|'--discard-prev'|'--double-next'|'--double-prev'} action
+ *
+ */
+
+const ACTIONS = {
+  SKIP_NEXT: '--discard-next',
+  SKIP_PREV: '--discard-prev',
+  DOUBLE_NEXT: '--double-next',
+  DOUBLE_PREV: '--double-prev',
+};
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
- * @param {Array} arr initial array
- * @returns {Array} transformed array
- * 
+ *
+ * @param {Array<number|action>} arr initial array
+ * @returns {Array<number>} transformed array
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr))
+    throw Error("'arr' parameter must be an instance of the Array!");
+
+  return arr.reduce((result, item, idx, initialArray) => {
+    switch (item) {
+      case ACTIONS.DOUBLE_NEXT:
+        return initialArray[idx + 1] == null
+          ? result
+          : result.concat(initialArray[idx + 1]);
+      case ACTIONS.DOUBLE_PREV:
+        return result[idx - 1] == null
+          ? result
+          : result.concat(result[idx - 1]);
+      case ACTIONS.SKIP_PREV:
+      case ACTIONS.SKIP_NEXT:
+        return result;
+      default:
+        return initialArray[idx - 1] === ACTIONS.SKIP_NEXT ||
+          initialArray[idx + 1] === ACTIONS.SKIP_PREV
+          ? result
+          : result.concat(item);
+    }
+  }, []);
 }
 
 module.exports = {
-  transform
+  transform,
 };
